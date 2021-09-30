a6e828d7e4d58aef00cf0a195a1514d7906f765f
Jon Torgeir Grini <torgeirgrini@gmail.com>
2021-09-30 11:47:56 +0200

    kommentarer

--- O2/a_star.py
+++ O2/a_star.py
@@ -1,10 +1,12 @@
 import Map
 
-### Av Eirik Runde Barlaug og Jon Torgeir Grini ifm. AI-intro øvinger. Overkommentert for stud-ass' skyld.
+### Av Eirik Runde Barlaug og Jon Torgeir Grini ifm. AI-intro øving 2. Overkommentert for stud-ass' skyld.
 
+
+#Oppretter en class Node 
 class Node(): 
     def __init__(self,state=None, g=None, h=None, f=None, parent=None, children=[]):
-        self.state = tuple(state) #x,y koordinater til noden, må ha tuple-form til senere
+        self.state = tuple(state) #x,y koordinater til noden som er en tuple
         self.g = g
         self.h = h
         self.f = f
@@ -14,48 +16,54 @@ class Node():
     def print_node(self): #til testing
         return f'{self.state[0]}, {self.state[1]}'
 
-def h_calculation(state, map_obj, type): #regner ut avstand til mål på et kart map_obj fra en node sin state state. Type bestemmer om euclidean eller manhattan skal taes i bruk
+#regner ut et estimat på avstanden fra en node til et mål på et kart map_obj. 
+#Type bestemmer om euclidean eller manhattan utregning skal tas i bruk
+def h_calculation(state, map_obj, type):
     goal = map_obj.get_goal_pos()
     if type == "manhattan": 
-        return abs(goal[0]-state[0]) + abs(goal[1]-state[1]) #se internett
-    if type == "euclidean":
-        return ((goal[0] - state[0])**2 + (goal[1] - state[1])**2)**0.5 #se internett
+        return abs(goal[0]-state[0]) + abs(goal[1]-state[1])
+    elif type == "euclidean":
+        return ((goal[0] - state[0])**2 + (goal[1] - state[1])**2)**0.5
     else:
         print("Type må være enten manhattan eller euclidean")
         return 0
 
+#cost_function returnerer kosten å gå fra en celle til en annen,
+#er f eks høyere hvis man går opp trapper enn ned trapper
 def cost_function(to_state, map_obj):
-    #returnerer kosten fra å gå fra en celle til en annen,
-    #er f eks høyere hvis man går opp trapper enn ned trapper, kun gjeldene i task > 2
     return map_obj.get_cell_value(to_state)
 
-
-def is_at_goal(state, goal_state): #sjekk for om tilstanden til en node (state) er i mål-posisjonen til mappet
+#Sjekke om en node sin state er vår goal-node sin state også
+def is_at_goal(state, goal_state): 
     return (state[0] == goal_state[0]) and (state[1] == goal_state[1])
 
-def init_start_node(start_state, h_function):
+#Initialiserer en start node
+def init_start_node(start_state, h_value):
     start_node = Node(
         state = start_state,
-        parent = None, #root-/start-noden har ingen forelder
-        children = []
+        parent = None #root-/start-noden har ingen forelder
     )
     start_node.g = 0
-    start_node.h = h_function
+    start_node.h = h_value
     start_node.f = start_node.h + start_node.g
     return start_node
 
+#Initialiserer en ny node
 def init_child_node(parent, child_state, h_function, map_obj):
     child_node = Node(
         parent=parent,
-        state=child_state,
-        children=[]
+        state=child_state
     )
+    #Tilegner den nye node g, h og f verdi
     child_node.g = parent.g + cost_function(child_node.state, map_obj) 
     child_node.h = h_calculation(child_node.state, map_obj, h_function)
     child_node.f = child_node.g + child_node.h
     return child_node
 
-def propagate_path_improvements(parent, map_obj, h_function): #kobler sammen child til billigste forelder dersom vi finner en lavere g-verdi via den forelderen
+#Rekurserer gjennom children og potensielt sett mange andre etterkommere til en parent P.
+#Dersom noen etterkommere ikke har P som sin beste forelder må dette endres og forplante seg til alle etterkommere av denne child
+#Forplantningen av beste forelder vil også kunne spre seg til children i OPEN-lista
+def propagate_path_improvements(parent, map_obj, h_function):
     for child in parent.children:
         new_g = parent.g + cost_function(child.state)
         if new_g < child.g:
@@ -63,11 +71,13 @@ def propagate_path_improvements(parent, map_obj, h_function): #kobler sammen chi
             child.g = new_g
             child.f = child.g + h_calculation(child.state, map_obj, h_function)
 
-def generate_children(parent, map_obj, h_function): #til å generere child-vector til en gitt node
+#Genererer en liste med children til en gitt node
+def generate_children(parent, map_obj):
     children_list = []
     for adjacent_tiles in [(0, -1), (0, 1), (-1, 0), (1, 0)]: #finner nabo. Skal kun ta hensyn til nord, sør, øst, vest
         child_pos_x = parent.state[0] + adjacent_tiles[0]
         child_pos_y = parent.state[1] + adjacent_tiles[1]
+        
         (w, h) = map_obj.int_map.shape #henter width og height for å sørge for at vi holder oss innenfor mappet
 
         #sjekke at child_pos er innenfor grensene på map_obj
@@ -81,10 +91,11 @@ def generate_children(parent, map_obj, h_function): #til å generere child-vecto
             continue
 
         new_node = Node(child_pos) #lager ny node av child, alle andre variabler beholder default verdiene fra Node-klassen
-        children_list.append(new_node) #fyller opp children-lista til foreldrenoden en etter en
+        children_list.append(new_node) #legger til new_node i en liste av children
     return children_list
 
-def attach_child(parent, child, h_function, map_obj): #funk til å koble barn til forelder, setter bare verdier
+#Koble en parent til en child, og fyller inn verdier for g, h, f for child
+def attach_child(parent, child, h_function, map_obj):
         child.parent = parent
         child.g = parent.g + cost_function(child.state, map_obj)
         child.h = h_calculation(child.state, map_obj, h_function)
@@ -101,9 +112,10 @@ def a_star(start, goal, h_function, map_obj):
     OPEN.append(start_node)
     memo[start_node.state] = start_node
 
-    while len(OPEN) > 0: #looper helt til vi har funnet goal-node
+    while len(OPEN) > 0: #looper så lenge det er elementer i OPEN
 
-        #sjekker de åpna cellene om det er noen som har bedre f-verdi, setter den til current node
+        #sjekker de cellene som skal åpnes om det er noen som har bedre f-verdi
+        #setter den med best f-verdi til current_node
         current_node = OPEN[0]
         current_i = 0 #trengs for å poppe riktig element fra OPEN
         for i, node in enumerate(OPEN): #bruker enumerate siden vi vil ha med oss indeksen
@@ -114,7 +126,7 @@ def a_star(start, goal, h_function, map_obj):
         OPEN.pop(current_i) 
         CLOSED.append(current_node)
         
-        #sjekker om vi har truffet mål enda
+        #sjekker om current_node er en mål-node
         if is_at_goal(current_node.state, goal):
             #går bakover langs alle barn->forelder->besteforelder osv. til vi er ved startnoden. Legger til alle koordinater i path slik at vi kan returnere den
             path = [] 
@@ -126,23 +138,31 @@ def a_star(start, goal, h_function, map_obj):
 
 
         #genererer barn til current_node
-        children = generate_children(current_node, map_obj, h_function)
+        children = generate_children(current_node, map_obj)
 
+        #går gjennom alle children til current_node én etter én
         for child in children:
+
+            #Dersom state til en child allerede eksisterer i en annen node betyr dette at vi noden er oppdaget fra før
+            #Da må vi bruke den noden som er funnet tidligere, slik at vi kan undersøke om den gamle eller nye noden har best g-verdi
             if child.state in memo:
                 child_node = memo[child.state]
+            #Dersom noden sin state ikke eksisterer i en annen node oppretter vi en ny node med denne staten og legger til i memo
             else:
                 child_node = init_child_node(current_node, child.state, h_function, map_obj) #hvis vi ikke har vært innom noden fra før og den finnes i memo, genererer vi en ny
                 memo[child_node.state] = child_node
             
+            #Legger til child_node som en child til current_node
             current_node.children.append(child_node)
 
-            if child_node not in OPEN and child_node not in CLOSED: #vi har ikke undersøkt denne noden enda
-                attach_child(current_node, child_node, h_function, map_obj) 
-                OPEN.append(child_node) 
-            elif current_node.g + cost_function(child_node.state, map_obj) < child_node.g: #hvis kosten via current er billigere så:
-                attach_child(current_node, child_node, h_function, map_obj)
-                if child_node in CLOSED:
+            if child_node not in OPEN and child_node not in CLOSED: #Betyr at vi ikke har undersøkt denne noden enda
+                attach_child(current_node, child_node, h_function, map_obj) #Fyller inn verdier for child
+                OPEN.append(child_node) #Legger til child i OPEN, slik at den åpnes senere
+            elif current_node.g + cost_function(child_node.state, map_obj) < child_node.g: #Betyr at kosten via current_node er billigere
+                attach_child(current_node, child_node, h_function, map_obj) #Fyller inn verdier for child
+                #dersom child_node ligger i CLOSED allerede må vi sjekke om vi kan forbedre g-verdien til child_node og 
+                # andre etterkommer via veien gjennom current_node
+                if child_node in CLOSED: 
                     propagate_path_improvements(child_node, map_obj, h_function) #hvis den også er i closed så?? hihi
 
 
